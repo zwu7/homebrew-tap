fbf0e2542ab48135b08ad7a3d9865d42b8f56e5c
zwu <zwu7@nd.edu>
2026-04-26 19:27:46 -0400

    Fix buzz-captions cask DSL

--- Casks/buzz-captions.rb
+++ Casks/buzz-captions.rb
@@ -1,12 +1,19 @@
-k "buzz-captions" do
-  arch arm: "ARM64", intel: "X64"
-
+cask "buzz-captions" do
   version "1.4.4"
-  sha256 arm:   "956d74ec3db341e04867a4dd727ed22fa8ec629762e505624dcce85a78eb94f8",
-         intel: "514edaa47841069c9f8953bdb0f3619167ed7a19e93427ec35014a89828d2fbb"
 
-  url "https://sourceforge.net/projects/buzz-captions/files/Buzz-#{version}-mac-#{arch}.dmg/download",
-      verified: "sourceforge.net/projects/buzz-captions/files/"
+  on_arm do
+    sha256 "956d74ec3db341e04867a4dd727ed22fa8ec629762e505624dcce85a78eb94f8"
+
+    url "https://sourceforge.net/projects/buzz-captions/files/Buzz-#{version}-mac-ARM64.dmg/download",
+        verified: "sourceforge.net/projects/buzz-captions/files/"
+  end
+  on_intel do
+    sha256 "514edaa47841069c9f8953bdb0f3619167ed7a19e93427ec35014a89828d2fbb"
+
+    url "https://sourceforge.net/projects/buzz-captions/files/Buzz-#{version}-mac-X64.dmg/download",
+        verified: "sourceforge.net/projects/buzz-captions/files/"
+  end
+
   name "Buzz"
   desc "Offline audio transcription and translation app powered by Whisper"
   homepage "https://github.com/chidiwilliams/buzz"
