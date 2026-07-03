6b853ef4347bbb641cf7b406006af13e888c81c7
zwu7 <88484720+zwu7@users.noreply.github.com>
2026-07-03 15:50:16 -0400

    Change download URL for Microsoft Word 2016
    Updated the download URL for Microsoft Word 2016 15.41.

--- Casks/microsoft-word-2016-1541.rb
+++ Casks/microsoft-word-2016-1541.rb
@@ -2,8 +2,8 @@ cask "microsoft-word-2016-1541" do
   version "15.41.17120500"
   sha256 "61d2e8dbc14e9418a566937bcbc883b4ef3ed06e54932a78cbe30352fac1b978"
 
-  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Word_#{version}_Updater.pkg",
-      verified: "officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/"
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Word_#{version}_Updater.pkg",
+    verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/"
   name "Microsoft Word 2016 15.41"
   desc "Word processor pinned to Office 2016 15.41 for legacy EndNote CWYW compatibility"
   homepage "https://www.microsoft.com/en-US/microsoft-365/word"
