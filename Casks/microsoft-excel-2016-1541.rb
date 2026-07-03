f2a4a78cce2551ec86401683fe20e09c31ff0ce4
zwu7 <88484720+zwu7@users.noreply.github.com>
2026-07-03 15:50:39 -0400

    Update download URL for Microsoft Excel 2016

--- Casks/microsoft-excel-2016-1541.rb
+++ Casks/microsoft-excel-2016-1541.rb
@@ -2,8 +2,8 @@ cask "microsoft-excel-2016-1541" do
   version "15.41.17120500"
   sha256 "69de1ce658ab75fc0a7223ff1c9298e1099b5018ce5678d98f646d7bb494c9f4"
 
-  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Updater.pkg",
-      verified: "officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/"
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Excel_#{version}_Updater.pkg",
+    verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/"
   name "Microsoft Excel 2016 15.41"
   desc "Spreadsheet software pinned to Office 2016 15.41"
   homepage "https://www.microsoft.com/en-US/microsoft-365/excel"
