b3778959ef39cc1810b15348f5752097db1ed336
zwu7 <88484720+zwu7@users.noreply.github.com>
2026-07-03 15:44:22 -0400

    Add Cask for Microsoft Excel 2016 version 15.41

---
+++ Casks/microsoft-excel-2016-1541.rb
@@ -0,0 +1,31 @@
+cask "microsoft-excel-2016-1541" do
+  version "15.41.17120500"
+  sha256 "69de1ce658ab75fc0a7223ff1c9298e1099b5018ce5678d98f646d7bb494c9f4"
+
+  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Updater.pkg",
+      verified: "officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/"
+  name "Microsoft Excel 2016 15.41"
+  desc "Spreadsheet software pinned to Office 2016 15.41"
+  homepage "https://www.microsoft.com/en-US/microsoft-365/excel"
+
+  livecheck do
+    skip "Pinned legacy Office 2016 15.41 build"
+  end
+
+  auto_updates false
+
+  conflicts_with cask: [
+    "microsoft-excel",
+    "microsoft-office",
+    "microsoft-office-businesspro",
+  ]
+
+  pkg "Microsoft_Excel_#{version}_Updater.pkg"
+
+  uninstall quit:    [
+              "com.microsoft.Excel",
+              "com.microsoft.autoupdate2",
+            ],
+            pkgutil: "com.microsoft.package.Microsoft_Excel.app",
+            delete:  "/Applications/Microsoft Excel.app"
+end
