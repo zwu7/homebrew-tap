4bb11ece3cdb81e936d77586c2a7e6c9a4eba585
zwu7 <88484720+zwu7@users.noreply.github.com>
2026-07-03 15:51:00 -0400

    Update PowerPoint download URL for 2016 version

--- Casks/microsoft-powerpoint-2016-1541.rb
+++ Casks/microsoft-powerpoint-2016-1541.rb
@@ -2,8 +2,8 @@ cask "microsoft-powerpoint-2016-1541" do
   version "15.41.17120500"
   sha256 "b531382a013a6846e8ce1943fe9828a11865ada584b3e98b2065258a5d0c3875"
 
-  url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Updater.pkg",
-      verified: "officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/"
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_PowerPoint_#{version}_Updater.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/"
   name "Microsoft PowerPoint 2016 15.41"
   desc "Presentation software pinned to Office 2016 15.41"
   homepage "https://www.microsoft.com/en-US/microsoft-365/powerpoint"
